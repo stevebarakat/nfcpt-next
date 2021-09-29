abbe5a80de57ec6e0efaf41762d105db53645eff
Steve <stevebarakat@gmail.com>
2021-09-29 11:54:05 -0400

    check in

--- components/MainMenu.js
+++ components/MainMenu.js
@@ -1,11 +1,30 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import styles from '../styles/MainMenu.module.css'
 import { FaCaretDown, FaBars, FaTimes } from 'react-icons/fa';
+import useOnClickOutside from '../hooks/useOnClickOutside';
 
 const MainMenu = () => {
-  const [toggle, setToggle] = useState(false);
+  const aboutEl = useRef();
+  const servicesEl = useRef();
+  const [toggleAbout, setToggleAbout] = useState(false);
+  const [toggleServices, setToggleServices] = useState(false);
+  useOnClickOutside(aboutEl, () => setToggleAbout(false));
+  useOnClickOutside(servicesEl, () => setToggleServices(false));
+
+  function handlePointerDown(e) {
+    console.log(e.currentTarget.id)
+    console.log(aboutEl.current.id)
+    console.log(servicesEl.current.id)
+    if (e.currentTarget.id === aboutEl.current.id) {
+      setToggleAbout(!toggleAbout)
+    }
+    if (e.currentTarget.id === servicesEl.current.id) {
+      setToggleServices(!toggleServices);
+    }
+  }
+
   function handleKeyDown(e) {
-    console.log(e.code)
+    console.log(ref.current)
     switch (e.code) {
       case "Space":
       case "Enter":
@@ -26,18 +45,18 @@ const MainMenu = () => {
         <FaBars />
       </a>
       <nav className={styles.nav} id="navbar">
-        <ul>
+        <ul onKeyDown={handleKeyDown}>
           <li><a href="#">Home</a></li>
-          <li><a href="#">Shop</a></li>
-          <li><a href="#">Blog</a></li>
-          <li><a href="#">Connect</a></li>
-          <li className={styles.dropdown}>
+          <li><a href="#">About</a></li>
+          <li><a href="#">First Visit</a></li>
+          <li><a href="#">Plans</a></li>
+          <li onPointerDown={handlePointerDown} id="about" className={styles.dropdown} ref={aboutEl}>
             <a href="#navbar"
-              onPointerDown={() => setToggle(!toggle)}
+
               onKeyDown={handleKeyDown}
-            >Services<FaCaretDown /></a>
+            ><div style={{ display: "flex" }}>About<FaCaretDown /></div></a>
             <ul onKeyDown={handleKeyDown}
-              style={!toggle ? { display: "none" } : { display: "block" }}
+              style={!toggleAbout ? { display: "none" } : { display: "block" }}
               className={styles.dropdownMenu}>
               <li><a href="#">Belle</a></li>
               <li><a href="#bubbles">Bubbles</a></li>
@@ -45,15 +64,19 @@ const MainMenu = () => {
               <li><a href="#">Fred</a></li>
             </ul>
           </li>
-          {/* <li className={styles.dropdown}>
-            <a href="#" onKeyDown={handleKeyDown}>About <FaCaretDown /></a>
-            <ul style={!toggle ? { display: "none" } : { display: "block" }} className={styles.dropdownMenu}>
+          <li onPointerDown={handlePointerDown} id="services" className={styles.dropdown} ref={servicesEl}>
+            <a href="#navbar"
+              onKeyDown={handleKeyDown}
+            ><div style={{ display: "flex" }}>Services<FaCaretDown /></div></a>
+            <ul onKeyDown={handleKeyDown}
+              style={!toggleServices ? { display: "none" } : { display: "block" }}
+              className={styles.dropdownMenu}>
               <li><a href="#">Belle</a></li>
-              <li><a href="#">Bubbles</a></li>
+              <li><a href="#bubbles">Bubbles</a></li>
               <li><a href="#">Dolores</a></li>
               <li><a href="#">Fred</a></li>
             </ul>
-          </li> */}
+          </li>
           <li><a href="#">Contact</a></li>
         </ul>
         <a className={styles.close} href="#" aria-label="Close main menu">
