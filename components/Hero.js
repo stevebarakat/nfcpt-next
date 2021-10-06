37927e30ea2e2d4179db0b6adafb245abd35c28b
Steve <stevebarakat@gmail.com>
2021-10-06 03:59:48 -0400

    fixed header

--- components/Hero.js
+++ components/Hero.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import styles from './hero.module.css'
+import Image from 'next/image';
+import slide1 from '../images/hero-slider-dark-1.jpg';
 
 const Hero = () => {
   return (
     <section className="section">
+
       <div className={styles.hero}>
+        <span className={styles.headline} style={{ zIndex: "9999" }} >Start your new normal</span>
+        <span className={styles.description}>Guiding accident victims to recovery and relief from back pain, neck pain, whiplash, migraines and more.<br />Accident recovery is what we do.</span>
         <div className={styles.heroImg}>
-          <h1>Start your new normal</h1>
-          <span className={styles.description}>Guiding accident victims to recovery and relief from back pain, neck pain, whiplash, migraines and more.<br />Accident recovery is what we do.</span>
+          <Image className={styles.heroImg} layout="fill" objectFit="cover" objectPosition="center" src={slide1} alt="Picture of the author" />
         </div>
       </div>
     </section>
---
+++ images/image-data.js
@@ -0,0 +1,5 @@
+export const images = [
+  "/hero-slider-1.jpg",
+  "https://d33wubrfki0l68.cloudfront.net/49de349d12db851952c5556f3c637ca772745316/cfc56/static/images/wallpapers/bridge-02@2x.png",
+  "https://d33wubrfki0l68.cloudfront.net/594de66469079c21fc54c14db0591305a1198dd6/3f4b1/static/images/wallpapers/bridge-01@2x.png"
+];
