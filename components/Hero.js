d2e0ccaa38a3b763275edb9d990324824a2de4bc
Steve <stevebarakat@gmail.com>
2021-10-07 01:18:41 -0400

    check in

--- components/Hero.js
+++ components/Hero.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './hero.module.css'
 import Image from 'next/image';
 import slide1 from '../images/happy-massage.jpg';
 
 const Hero = () => {
+  const [height, setHeight] = useState(null);
+
+  useEffect(() => {
+    window.onload = setHeight(window.innerHeight);
+    window.onresize = setHeight(window.innerHeight);
+  });
+
   return (
     <section className="container">
-      <div className={styles.hero}>
+      <div style={{ height: height }} className={styles.hero}>
         <span className={styles.headline}>Rest Assured</span>
         <span className={styles.description}>We guide accident victims to recovery and relief from back pain, neck pain, whiplash, headaches and more. <span className="bold">Accident recovery is what we do.</span></span>
         <div className={styles.heroImg}>
