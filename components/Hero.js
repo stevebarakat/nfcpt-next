0c60ac395abdeef3b81b15bf29aed25c4cc5876a
Steve <stevebarakat@gmail.com>
2021-10-09 15:54:04 -0400

    working on bg imgs

--- components/Hero.js
+++ components/Hero.js
@@ -1,6 +1,6 @@
 import styles from './hero.module.css'
 import Image from 'next/image';
-import slide1 from '../images/hero-1.jpg';
+import hero from '../images/hero-1.jpg';
 
 const Hero = () => {
   return (
@@ -8,7 +8,7 @@ const Hero = () => {
       <div className={styles.hero}>
         <span className={styles.headline}>Rest Assured</span>
         <span className={styles.description}>We guide accident victims to recovery and relief from back pain, neck pain, whiplash, headaches and more. <span className="bold">Accident recovery is what we do.</span></span>
-        <Image layout="fill" objectFit="cover" objectPosition="center" src={slide1} alt="Picture of the author" />
+        <Image layout="fill" objectFit="cover" objectPosition="center" src={hero} alt="Picture of the author" />
       </div>
     </section>
   )
