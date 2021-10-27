501464728452e059570a17d0e837635f166fcc3d
Steve <stevebarakat@gmail.com>
2021-10-27 17:47:48 -0400

    dangerouslySetInnerHTML

--- pages/[slug].js
+++ pages/[slug].js
@@ -5,7 +5,7 @@ export default function Page({ page }) {
   return (
     <div>
       <h1>{page.title}</h1>
-      <p>{page.content}</p>
+      <p dangerouslySetInnerHTML={{ __html: page.content }}></p>
     </div>
   );
 }
