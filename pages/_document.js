4bdf7707a3b984e1c3c7009601056dc4c985c3a0
Steve <stevebarakat@gmail.com>
2021-11-10 15:13:31 -0500

    added google fonts

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,19 @@ class WebDocument extends Document {
     return (
       <Html lang="en-US">
         <Head>
+          <link
+            rel="preconnect"
+            href="https://fonts.googleapis.com"
+          />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossorigin
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
           <script
             dangerouslySetInnerHTML={{
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
