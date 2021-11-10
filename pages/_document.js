63e56cd7e4916f8abd5273a59b0dac831918cea9
Steve <stevebarakat@gmail.com>
2021-11-10 17:00:04 -0500

    change font display back to swap

--- pages/_document.js
+++ pages/_document.js
@@ -25,7 +25,7 @@ class WebDocument extends Document {
             crossOrigin
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:wght@100;200;300;400;500;600;700;800&display=optional"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:wght@100;200;300;400;500;600;700;800&display=swap"
             rel="stylesheet"
           />
           <NextScript
